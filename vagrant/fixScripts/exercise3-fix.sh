dfd4f2b4bb0c7942177a7e00f52ea4ef317b8442
Stav C <stav.c@cg.solutions>
2021-07-14 17:58:25 +0300

    done all exercises

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-#add fix to exercise3 here
+head -n -3 /etc/apache2/sites-available/000-default.conf | sudo tee /etc/apache2/sites-available/000-default.conf
+echo '        <Location "/">
+          Allow from all
+          Order Deny,Allow
+        </Location>' | sudo tee -a /etc/apache2/sites-available/000-default.conf
+sudo systemctl reload apache2
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+DEST=${!#}  
+DEST_HOST=""
+FILES=${@:1:$#-1}
+
+if  (($# >= 2))
+then
+    case $HOSTNAME in
+        (server1) DEST_HOST="server2";;
+        (server2) DEST_HOST="server1";;
+    esac
+
+    scp -q $FILES $USER@$DEST_HOST:$DEST$SOURCE
+    echo $(du -b -c $FILES | tail -1 | cut -f1)
+    cat ~/.ssh/id_rsa | sudo tee /root/.ssh/id_rsa && sudo chmod 600 /root/.ssh/id_rsa
+    cat ~/.ssh/authorized_keys | sudo tee -a /root/.ssh/authorized_keys && sudo chmod 600 /root/.ssh/authorized_keys
+else
+    echo "Number of arguments need to be greater than 2"
+    exit 1
+fi
