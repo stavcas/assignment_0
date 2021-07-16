2e4b701097148baab2ec96e76b25b716ff86fe6c
Stav C <stav.c@cg.solutions>
2021-07-16 11:36:26 +0300

    final fix

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
 echo "192.168.100.11 server2" | sudo tee -a /etc/hosts  > /dev/null
+cat /home/vagrant/.ssh/authorized_keys | sudo tee -a /root/.ssh/authorized_keys > /dev/null && sudo chmod 600 /root/.ssh/authorized_keys && sudo chown root:root /root/.ssh/authorized_keys
